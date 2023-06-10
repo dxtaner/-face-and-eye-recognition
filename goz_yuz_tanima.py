786837c0e4e9f45d29723f243bfd50eeae981949
Taner Özer <tanerozer16@gmail.com>
2023-06-10 17:37:13 +0300

    Update goz_yuz_tanima.py

--- goz_yuz_tanima.py
+++ goz_yuz_tanima.py
@@ -1,29 +1,33 @@
 import cv2
 
-yuz_cascade=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-goz_cascade=cv2.CascadeClassifier('haarcascade_eye.xml')
+def detect_faces_eyes():
+    yuz_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    goz_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
-kamera=cv2.VideoCapture(0)
+    kamera = cv2.VideoCapture(0)
 
-while True:
-    cm,goruntu=kamera.read()
+    while True:
+        ret, goruntu = kamera.read()
 
-    griton=cv2.cvtColor(goruntu,cv2.COLOR_BGR2GRAY) #gritonlama
-    yuzler=yuz_cascade.detectMultiScale(griton,1.3,5)
+        griton = cv2.cvtColor(goruntu, cv2.COLOR_BGR2GRAY)
+        yuzler = yuz_cascade.detectMultiScale(griton, 1.3, 5)
 
-    for (x,y,w,h) in yuzler:
-        cv2.rectangle(goruntu,(x,y),(x+w,y+h),(0,0,255),3)
-        griton=griton[y:y+h,x:x+w]
-        renkli=goruntu[y:y+h,x:x+w]
-        gozler=goz_cascade.detectMultiScale(griton)
+        for (x, y, w, h) in yuzler:
+            cv2.rectangle(goruntu, (x, y), (x + w, y + h), (0, 0, 255), 3)
+            yuz_griton = griton[y:y + h, x:x + w]
+            yuz_renkli = goruntu[y:y + h, x:x + w]
+            gozler = goz_cascade.detectMultiScale(yuz_griton)
 
-        for(ex,ey,ew,eh) in gozler:
-            cv2.rectangle(renkli,(ex,ey),(ex+ew,ey+eh),(0,255,255),2)
+            for (ex, ey, ew, eh) in gozler:
+                cv2.rectangle(yuz_renkli, (ex, ey), (ex + ew, ey + eh), (0, 255, 255), 2)
 
-    cv2.imshow('goruntu',goruntu)
+        cv2.imshow('Görüntü', goruntu)
 
-    if cv2.waitKey(13) &0xFF==ord('q'):
-        break
-    
-kamera.release()
-cv2.destroyAllWindows()
+        if cv2.waitKey(13) & 0xFF == ord('q'):
+            break
+
+    kamera.release()
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    detect_faces_eyes()
